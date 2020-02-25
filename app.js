4f6fb475a72c1774200ce1cc5418bd934294f749
yjwang12 <yjwang0012@gmail.com>
2020-02-25 20:58:44 +0800

    watch

--- app.js
+++ app.js
@@ -58,6 +58,32 @@ App({
       'number': wx.getStorageSync('driverNumber'),
     },
     that: ''
+  },
+
+
+  /**
+   * 设置监听器
+   */
+
+  setWatcher(data, page) { // 接收index.js传过来的data对象和watch对象
+    var watch = page.watch
+    Object.keys(watch).forEach(v => { // 将watch对象内的key遍历
+      this.observe(data, v, watch[v], page); // 监听data内的v属性，传入watch内对应函数以调用
+    })
+  },
+  observe(obj, key, watchFun, page) {
+    var val = obj[key]; // 给该属性设默认值
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      set: function (value) {
+        val = value;
+        watchFun(value, val); // 赋值(set)时，调用对应函数
+      },
+      get: function () {
+        return val;
+      }
+    })
   }
 })
 
